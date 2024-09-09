4e3a06c835e0e3ea1c4775f12cc96e2a62924e1c
Samuel Easton (RIT Student) <sge3418@rit.edu>
2024-09-09 14:51:23 -0400

    Accept Header Inclass Work

--- src/responses.js
+++ src/responses.js
@@ -3,13 +3,21 @@ const fs = require('fs'); // pull in the file system module
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const respond = (request, response, content, type) => {
-  response.writeHead(200, { 'Content-Type': type });
+  response.writeHead(200, {
+    'Content-Type': type,
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+
+  });
   response.write(content);
   response.end();
 };
 
 const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
+};
+
+const getCats = (request, response) => {
+  
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -4,12 +4,21 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+  '/': responseHandler.getIndex,
+  '/cats': responseHandler.getCats,
+  index: responseHandler.getIndex,
 };
 
 const onRequest = (request, response) => {
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-};
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response);
+  } else {
+    urlStruct.index(request, response);
+  }
+}; // end onRequest
 
 http.createServer(onRequest).listen(port, () => {
   console.log(`Listening on 127.0.0.1: ${port}`);
